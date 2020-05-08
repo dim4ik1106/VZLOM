7af9fbdb8737d5f3a1d67f36f8dcce24375d455a
Dmitry Shmatkov <dim4ik11061106@gmail.com>
2020-05-08 08:15:55 +0300

    Added scripts for mobile version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 // $(document).ready(function () {
 let hackButton = document.getElementById('hack-button'),
+    mobileHackButton = document.getElementById('mobile-hack-button'),
+    mobileNextButton = document.getElementById('mobile-next-button'),
     hackInput = document.getElementById('hack-input'),
     targetId = document.getElementById('target-id'),
+    mobileTargetId = document.getElementById('mobile-target-id'),
     targetName = document.getElementById('target-name'),
+    mobileTargetName = document.getElementById('mobile-target-name'),
     targetAvatar = document.getElementById('target-avatar'),
+    mobileTargetAvatar = document.getElementById('mobile-target-avatar'),
     progressbar1 = document.getElementById('progress-bar-1'),
     progressbar2 = document.getElementById('progress-bar-2'),
+    mobileProgressbar1 = document.getElementById('mobile-progress-bar-1'),
+    mobileProgressbar2 = document.getElementById('mobile-progress-bar-2'),
     potentialPasscode = document.getElementById('potential-passcode'),
+    mobilePotentialPasscode = document.getElementById('mobile-potential-passcode'),
     step1 = document.getElementById('step1'),
     step2 = document.getElementById('step2'),
+    mobileStep1 = document.getElementById('mobile-step1'),
+    mobileStep2 = document.getElementById('mobile-step2'),
     vkDomainPattern = 'vk.com/',
     targetLink,
     targetProfileId,
@@ -17,6 +27,7 @@ let hackButton = document.getElementById('hack-button'),
     targetSurname,
     targetPhoto100,
     targetPhoto200,
+    isItDesktop,
     gray = "#5D5D60",
     white = '#ffffff';
 
@@ -30,12 +41,26 @@ $(hackInput).keyup(function (e) {
 });
 
 
+if ($(window).width() <= '600') {
+    isItDesktop = false;
+} else {
+    isItDesktop = true;
+}
+
+
 $(hackButton).click(function (e) {
     e.preventDefault();
     if (hackInput.value.includes(vkDomainPattern)) {
+
         $('.start-hack').hide();
-        $('.hack-started').show();
-        $('.hack-started').css('display', 'flex');
+
+        if (isItDesktop) {
+            $('.hack-started').show();
+            $('.hack-started').css('display', 'flex');
+        } else {
+            $('.mob-hack-started').show();
+            $('.mob-hack-started').css('display', 'flex');
+        }
 
         targetLink = hackInput.value;
 
@@ -62,15 +87,28 @@ $(hackButton).click(function (e) {
 
                 targetName.innerHTML = targetProfileName + ' ' + targetSurname + ' (id=' + targetProfileId + ')';
                 targetAvatar.src = targetPhoto200;
+                mobileTargetName.innerHTML = targetProfileName + ' ' + targetSurname + ' (id=' + targetProfileId + ')';
+                mobileTargetAvatar.src = targetPhoto200;
             }
         });
 
+        if (isItDesktop) {
+            setTimeout(progressPlusOne, 2000, progressbar1, 80);
+            setTimeout(changeColor, 2000, step1, white);
+        }
 
+        mobileTargetId.innerHTML = targetLink;
         targetId.innerHTML = targetLink;
-        // progressPlusOne(progressbar1, 80);
-        setTimeout(progressPlusOne, 2000, progressbar1, 80);
-        setTimeout(changeColor, 2000, step1, white);
     }
+});
+
+$(mobileNextButton).click(function (e) {
+    e.preventDefault();
+    $('.mob-information__container').hide();
+    $('.mob-first-step').show();
+    $('.mob-first-step').css('display', 'flex');
+    progressPlusOne(mobileProgressbar1, 80);
+    changeColor(mobileStep1, white);
 });
 
 
@@ -83,8 +121,8 @@ function changeColor(elem, color) {
 function progressPlusOne(bar, speed) {
     var width = 1;
     var interval = setInterval(frame, speed);
-    
-    
+
+
     function frame() {
         if (width >= 100) {
             clearInterval(interval);
@@ -92,6 +130,21 @@ function progressPlusOne(bar, speed) {
                 changeColor(step1, gray);
                 setTimeout(progressPlusOne, 2000, progressbar2, 140);
                 setTimeout(changeColor, 2000, step2, white);
+                setTimeout(function () {
+                    $('.potential-passcodes').show();
+                }, 2000);
+            }
+
+            if (bar == mobileProgressbar1) {
+                $('.mob-first-step').hide();
+                $('.mob-second-step').show();
+                $('.mob-second-step').css('display', 'flex');
+                $('.potential-passcodes').show();
+                progressPlusOne(mobileProgressbar2, 140);
+            }
+
+            if (bar == progressbar2 || bar == mobileProgressbar2) {
+                $('.potential-passcodes').hide();
             }
         } else {
             width++;
@@ -129,6 +182,7 @@ function generatePass() {
     }
 
     potentialPasscode.innerHTML = result;
+    mobilePotentialPasscode.innerHTML = result;
 
     setTimeout('generatePass()', 20);
 }
